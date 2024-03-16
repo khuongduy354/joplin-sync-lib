f936d820f8f19421f9fad103172e10ef25989cb2
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-03-16 22:22:13 +0700

    feat: helpers + singleton

---
+++ Locks/Locks.ts
@@ -0,0 +1,19 @@
+export enum LockType {
+	None = 0,
+	Sync = 1,
+	Exclusive = 2,
+}
+
+export enum LockClientType {
+	Desktop = 1,
+	Mobile = 2,
+	Cli = 3,
+}
+
+export interface Lock {
+	id?: string;
+	type: LockType;
+	clientType: LockClientType;
+	clientId: string;
+	updatedTime?: number;
+}
---
+++ helpers/logger.ts
@@ -0,0 +1,6 @@
+export const logger = { 
+    info: console.info, 
+    error: console.error, 
+    warn: console.warn, 
+    debug: console.debug
+}
