937bca332a79d18e9ac9fed4d7112f6ca0a1b961
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-06-04 23:21:09 +0700

    test: utils for 1st passed test case

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   // testMatch: ["**/*.test.js"],
 
-  // testPathIgnorePatterns: [
-  //   "<rootDir>/node_modules/",
-  //   "<rootDir>/rnInjectedJs/",
-  //   "<rootDir>/vendor/",
-  // ],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/rnInjectedJs/",
+    "<rootDir>/vendor/",
+    "<rootDir>/sample_profile_directory/",
+  ],
 
   testEnvironment: "node",
 
   transform: {
-    "\\.(ts|tsx)$": "ts-jest",
+    "\\.(ts|tsx)$": ["ts-jest", { diagnostics: false }],
   },
 
   // setupFilesAfterEnv: [`${__dirname}/jest.setup.js`],
