1d5e47dec60c4b6107468fe79198cf0c88d780dc
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2025-09-27 09:32:40 +0700

    chore:  remove unused models files

--- src/Model/index.ts
+++ src/Model/index.ts
@@ -3,7 +3,3 @@ export {
   ModelType,
   type BaseItemData,
 } from "./BaseModel";
-export { default as Note, type NoteData } from "./note";
-export { default as Folder, type FolderData } from "./folder";
-export { default as Resource, type ResourceData } from "./resource";
-export { default as Tag, type TagData } from "./tag";
